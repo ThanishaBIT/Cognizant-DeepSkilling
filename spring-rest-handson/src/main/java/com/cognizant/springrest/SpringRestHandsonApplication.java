3e18cdcced057a7f806b9b0fb2d012732dd0f1f8
Thanisha G <thanisha293@gmail.com>
2026-07-24 12:29:00 +0530

    Add Spring REST Exercise 2 - Load Country from Spring Configuration XML

--- spring-rest-handson/src/main/java/com/cognizant/springrest/SpringRestHandsonApplication.java
+++ spring-rest-handson/src/main/java/com/cognizant/springrest/SpringRestHandsonApplication.java
@@ -3,11 +3,17 @@ package com.cognizant.springrest;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.cognizant.springrest.service.CountryService;
+
 @SpringBootApplication
 public class SpringRestHandsonApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringRestHandsonApplication.class, args);
-	}
 
+		SpringApplication.run(SpringRestHandsonApplication.class, args);
+
+		CountryService service = new CountryService();
+
+		System.out.println(service.getCountry());
+	}
 }
